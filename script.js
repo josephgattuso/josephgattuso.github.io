0e2aae941a122d2a213bfb27f3d5097d064f8be8
Joseph Gattuso <joseph.gattuso@gmail.com>
2021-01-26 19:58:15 +0000

    Refactor carousel

--- script.js
+++ script.js
@@ -1,49 +1,4 @@
-// Carousel
-
-const imgs = document.getElementById("imgs");
-const leftBtn = document.getElementById("left");
-const rightBtn = document.getElementById("right");
-const img = document.querySelectorAll("#imgs img");
-
-let idx = 0;
-
-// 2000ms = 2secs
-let interval = setInterval(run, 2000);
-
-function run() {
-  idx++;
-  changeImage();
-}
-
-function changeImage() {
-  if (idx > img.length - 1) {
-    idx = 0;
-  } else if (idx < 0) {
-    idx = img.length - 1;
-  }
-
-  // carousel animation
-  imgs.style.transform = `translateX(${-idx * 420}px)`;
-}
-
-function resetInterval() {
-  clearInterval(interval);
-  interval = setInterval(run, 2000);
-}
-
-rightBtn.addEventListener("click", () => {
-  idx++;
-
-  changeImage();
-  resetInterval();
-});
-
-leftBtn.addEventListener("click", () => {
-  idx--;
-
-  changeImage();
-  resetInterval();
-});
+import data from "./data.js";
 
 // Navbar
 
@@ -55,3 +10,81 @@ function toggleMenu() {
     myBtn.className = "topnav";
   }
 }
+
+// Carousel
+
+const container = document.querySelector(".slide-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+// if length is 1 hide buttons
+if (data.length === 1) {
+  nextBtn.style.display = "none";
+  prevBtn.style.display = "none";
+}
+// if length is 2, add copies of slides
+let projects = [...data];
+if (data.length === 2) {
+  projects = [...data, ...data];
+}
+container.innerHTML = projects
+  .map((project, slideIndex) => {
+    const { img, name, description, url, repo } = project;
+    let position = "next";
+    if (slideIndex === 0) {
+      position = "active";
+    }
+    if (slideIndex === projects.length - 1) {
+      position = "last";
+    }
+    if (data.length <= 1) {
+      position = "active";
+    }
+    return `
+    <article class="slide ${position}">
+      <a href=${url} target="_blank' rel="noopener noreferrer">
+        <img src=${img} class="img" title=${name} alt="${name}"/>
+      </a>
+      <h4>${name}</h4>
+      <p class="description">
+      ${description}
+      </p>
+      <a href=${repo} target="_blank' rel="noopener noreferrer">View Source</a>
+      <a href=${url} target="_blank' rel="noopener noreferrer">Live Demo</a>
+    </article>
+    `;
+  })
+  .join("");
+
+const startSlider = type => {
+  // get all three slides active,last next
+  const active = document.querySelector(".active");
+  const last = document.querySelector(".last");
+  let next = active.nextElementSibling;
+  if (!next) {
+    next = container.firstElementChild;
+  }
+  active.classList.remove(["active"]);
+  last.classList.remove(["last"]);
+  next.classList.remove(["next"]);
+
+  if (type === "prev") {
+    active.classList.add("next");
+    last.classList.add("active");
+    next = last.previousElementSibling;
+    if (!next) {
+      next = container.lastElementChild;
+    }
+    next.classList.remove(["next"]);
+    next.classList.add("last");
+    return;
+  }
+  active.classList.add("last");
+  last.classList.add("next");
+  next.classList.add("active");
+};
+nextBtn.addEventListener("click", () => {
+  startSlider();
+});
+prevBtn.addEventListener("click", () => {
+  startSlider("prev");
+});
