6c344cfa88ba2f9a418ac87097e4e89de61ce276
Joseph Gattuso <joseph.gattuso@gmail.com>
2021-01-28 13:28:43 +0000

    Cleanup useless code

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-import data from "./data.js";
-
-// Navbar
-
-function toggleMenu() {
-  const myBtn = document.getElementById("myTopnav");
-  if (myBtn.className === "topnav") {
-    myBtn.className += " responsive";
-  } else {
-    myBtn.className = "topnav";
-  }
-}
-
 // Carousel
 
 const sliderData = [
