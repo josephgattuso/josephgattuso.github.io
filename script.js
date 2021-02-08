5cbd73ae66f1330a2b1f3ff7cc346624fcfc7157
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-02-08 15:49:36 +0000

    Fix typo in slide function

--- script.js
+++ script.js
@@ -441,7 +441,7 @@ const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 
 const slideInterval = setInterval(() => {
-  slides[curerntSlide].classList.remove('show');
+  slides[currentSlide].classList.remove('show');
   currentSlide = (currentSlide + 1) % slides.length;
   slides[currentSlide].classList.add('show');
 }, 5000);
