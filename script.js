23442114e6439367961d880ac71c045fc2c5bc19
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-02-18 16:15:37 +0000

    Add dark-mode theme

--- script.js
+++ script.js
@@ -389,3 +389,31 @@ menuBars.addEventListener('click', toggleNav);
 navItems.forEach(nav => {
   nav.addEventListener('click', toggleNav);
 });
+
+// Nav section
+
+// Set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+
+// Toggle between light and dark theme
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-light');
+  } else {
+    setTheme('theme-dark');
+  }
+}
+
+// Set theme on initial load
+(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark');
+    document.getElementById('slider').checked = false;
+  } else {
+    setTheme('theme-light');
+    document.getElementById('slider').checked = true;
+  }
+})();
