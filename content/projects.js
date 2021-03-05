5212973d63f6199d4a207e9bf5fcd2ab42d5b2bf
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-05 13:18:17 +0000

    Fix broken link

--- content/projects.js
+++ content/projects.js
@@ -61,7 +61,7 @@ const projects = [
     desc: 'Recreating a paint app with HTML canvas',
     demo: 'https://josephgattuso.github.io/js-projects-v2/paint-clone',
     repo:
-      'https://github.com/josephgattuso/js-projects/tree-v2/master/paint-clone',
+      'https://github.com/josephgattuso/js-projects-v2/tree/master/paint-clone',
   },
   {
     name: 'Movie App',
