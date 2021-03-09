922fe7073e11b0b727a2b121f20f3cccce41a7bb
Joseph Gattuso <19511143+josephgattuso@users.noreply.github.com>
2021-03-09 15:01:08 +0000

    Refactor theme toggle into components

--- app.js
+++ app.js
@@ -110,51 +110,51 @@ navItems.forEach(nav => {
 
 // Theme Toggle
 
-let darkMode = localStorage.getItem('darkMode');
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-const aboutImg = document.getElementById('about-img');
+// let darkMode = localStorage.getItem('darkMode');
+// const darkModeToggle = document.querySelector('#dark-mode-toggle');
+// const aboutImg = document.getElementById('about-img');
 
-function imageMode(color) {
-  aboutImg.src = `images/about_${color}.webp`;
-}
+// function imageMode(color) {
+//   aboutImg.src = `images/about_${color}.webp`;
+// }
 
-const enableDarkMode = () => {
-  // 1. Add the class to the body
-  document.body.classList.add('darkmode');
-  // 2. Update darkMode in localStorage
-  localStorage.setItem('darkMode', 'enabled');
-  // 3. Update About section image
-  imageMode('dark');
-};
+// const enableDarkMode = () => {
+//   // 1. Add the class to the body
+//   document.body.classList.add('darkmode');
+//   // 2. Update darkMode in localStorage
+//   localStorage.setItem('darkMode', 'enabled');
+//   // 3. Update About section image
+//   imageMode('dark');
+// };
 
-const disableDarkMode = () => {
-  // 1. Remove the class from the body
-  document.body.classList.remove('darkmode');
-  // 2. Update darkMode in localStorage
-  localStorage.setItem('darkMode', null);
-  // 3. Update About section image
-  imageMode('light');
-};
+// const disableDarkMode = () => {
+//   // 1. Remove the class from the body
+//   document.body.classList.remove('darkmode');
+//   // 2. Update darkMode in localStorage
+//   localStorage.setItem('darkMode', null);
+//   // 3. Update About section image
+//   imageMode('light');
+// };
 
-// If the user already visited and enabled darkMode
-// start things off with it on
-if (darkMode === 'enabled') {
-  enableDarkMode();
-}
+// // If the user already visited and enabled darkMode
+// // start things off with it on
+// if (darkMode === 'enabled') {
+//   enableDarkMode();
+// }
 
-// When someone clicks the button
-darkModeToggle.addEventListener('click', () => {
-  // get their darkMode setting
-  darkMode = localStorage.getItem('darkMode');
+// // When someone clicks the button
+// darkModeToggle.addEventListener('click', () => {
+//   // get their darkMode setting
+//   darkMode = localStorage.getItem('darkMode');
 
-  // if it not current enabled, enable it
-  if (darkMode !== 'enabled') {
-    enableDarkMode();
-    // if it has been enabled, turn it off
-  } else {
-    disableDarkMode();
-  }
-});
+//   // if it not current enabled, enable it
+//   if (darkMode !== 'enabled') {
+//     enableDarkMode();
+//     // if it has been enabled, turn it off
+//   } else {
+//     disableDarkMode();
+//   }
+// });
 
 // Image Carousel
 
@@ -229,4 +229,4 @@ darkModeToggle.addEventListener('click', () => {
 
 //   //adds css class to show current slide
 //   slides[currentSlide].className = 'slide show';
-// };s
+// };
